7cbe8afa69fd965ccf310c92f81aceb40fd0ec5b
SongHye <hungern95@gmail.com>
2016-06-24 22:02:48 +0900

    전체화면 추가

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,1 @@
 console.log('my-note app.js');
-
-function FullScreen(document) {
-  if(document.requestFullScreen) {
-    document.requestFullScreen();
-  } else if(document.requestFullScreen){
-    document.requestFullScreen();
-  } else if(document.webkitRequestFullScreen){
-    document.webkitRequestFullScreen();
-  } 
-}
